c76f056d39ff359b76d45dffc90701e5fc952f54
midhorner <horner.andrew.e@gmail.com>
2021-03-11 16:41:33 -0500

    forms and modals

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,47 @@ $(function() {
     $(this).prev('small').addClass('hide');
   });
 });
+
+function cateredEvent() {
+  $("#eventsButtons").toggleClass("hidden");
+  $(".catered-event").toggleClass("hidden");
+  $("#typeOfEvent").toggleClass("hidden");
+}
+
+$(function(){
+  $("#cateringButton").click(cateredEvent);
+  $("#hostingButton").click(hostedEvent);
+});
+
+function hostedEvent() {
+  $("#eventsButtons").toggleClass("hidden");
+  $(".hosted-event").toggleClass("hidden");
+  $("#typeOfEvent").toggleClass("hidden");
+}
+
+$(function(){
+  $("#catered-choice").click(cateredEvent);
+  $("#hosted-choice").click(hostedEvent);
+});
+
+$(function(){
+  $('input[name="rentingRadios"]').click(function(){
+    let radioValue = $('input[name="rentingRadios"]:checked').val();
+    if (radioValue === "yes") {
+      $(".size-warning").removeClass("hidden");
+    } else {
+      $(".size-warning").addClass("hidden");
+    }
+  });
+});
+
+$(function(){
+  $('input[name="cateringRadios"]').click(function(){
+    let radioValue = $('input[name="cateringRadios"]:checked').val();
+    if (radioValue === "on-site") {
+      $("#cateringNeeds").removeClass("hidden");
+    } else {
+      $("#cateringNeeds").addClass("hidden");
+    }
+  });
+});
