2af6add5ea8f80ebdfe5a7efe86ead12a21481a6
midhorner <horner.andrew.e@gmail.com>
2021-03-17 16:25:45 -0400

    style changes, cards, everything. big commit. forgot to commit my last set of changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,42 @@
 $(function(){
-  $(".navbar-toggler").click(function(){
+  $(".navbar-toggler").on("click", function(){
     $(".bars").toggleClass("hidden");
     $(".close-icon").toggleClass("hidden");
   });
-});
 
-$(function() {
   $('.form-group :input')
-  .focus(function() {
+  .on("focus", function() {
       $(this).prev('small').removeClass('hide');
     })
-  .blur(function() {
+  .on("blur", function() {
     $(this).prev('small').addClass('hide');
   });
+  
+  $("#cateringButton").on("click", cateredEvent);
+  $("#hostingButton").on("click", hostedEvent);
+
+  $("#cateredClose").on("click", cateredEvent);
+  $("#hostedClose").on("click", hostedEvent);
+
+  $('input[name="rentingRadios"]').on("click", function(){
+    let radioValue = $('input[name="rentingRadios"]:checked').val();
+    if (radioValue === "yes") {
+      $(".size-warning").removeClass("hidden");
+    } else {
+      $(".size-warning").addClass("hidden");
+    }
+  });
+
+  $('input[name="cateringRadios"]').on("click", function(){
+    let radioValue = $('input[name="cateringRadios"]:checked').val();
+    if (radioValue === "on-site") {
+      $("#cateringNeeds").removeClass("hidden");
+    } else {
+      $("#cateringNeeds").addClass("hidden");
+    }
+  });
 });
+
 
 function cateredEvent() {
   $("#eventsButtons").toggleClass("hidden");
@@ -27,34 +50,3 @@ function hostedEvent() {
   $("#typeOfEvent").toggleClass("hidden");
 }
 
-$(function(){
-  $("#cateringButton").click(cateredEvent);
-  $("#hostingButton").click(hostedEvent);
-});
-
-$(function(){
-  $("#catered-choice").click(cateredEvent);
-  $("#hosted-choice").click(hostedEvent);
-});
-
-$(function(){
-  $('input[name="rentingRadios"]').click(function(){
-    let radioValue = $('input[name="rentingRadios"]:checked').val();
-    if (radioValue === "yes") {
-      $(".size-warning").removeClass("hidden");
-    } else {
-      $(".size-warning").addClass("hidden");
-    }
-  });
-});
-
-$(function(){
-  $('input[name="cateringRadios"]').click(function(){
-    let radioValue = $('input[name="cateringRadios"]:checked').val();
-    if (radioValue === "on-site") {
-      $("#cateringNeeds").removeClass("hidden");
-    } else {
-      $("#cateringNeeds").addClass("hidden");
-    }
-  });
-});
