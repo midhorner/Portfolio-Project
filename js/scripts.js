5cd203fd64274b187d0f7392021982109953168f
midhorner <horner.andrew.e@gmail.com>
2021-03-18 19:32:20 -0400

    merch cards and shopping modal w/ overly complicated jquery action

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,48 @@ $(function(){
       $("#cateringNeeds").addClass("hidden");
     }
   });
+
+  $('#shopModal').on('show.bs.modal', function (event) {
+    const trigger = $(event.relatedTarget);
+    let type = trigger.data('type');
+    let imageSource = trigger.find('img').attr('src');
+    let title = trigger.find('.card-title').text();
+    const modal = $(this);
+    modal.find('#modalImage').html(`<img src="${imageSource}" class="shop-img img-fluid"></img`);
+    modal.find('.modal-title').text(title);
+    if (type !== 'shirt') {
+      modal.find('.shirt-sizes').addClass('hidden');
+      $('#shopQuantity').on('click', function() {
+        let quantity = $('#shopQuantity').val();
+        $('.total-cost').html(`$${((quantity * 18.00) * 1.07).toFixed(2)}`);
+      })
+    } else {
+      modal.find('.shirt-sizes').removeClass('hidden');
+      $('#shopQuantity').on('click', function() {
+        let quantity = $('#shopQuantity').val();
+        $('.total-cost').html(`$${((quantity * 16.00) * 1.07).toFixed(2)}`);
+      })
+    }
+  });
+
+  $('.modal').on('hide.bs.modal', function () {
+    $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
+    $(':checkbox, :radio').prop('checked', false);
+    $('.total-cost').html('');
+    $('#shopSize').val($('#shopSize option:first').val());
+    $('#resPartySize').val($('#resPartySize option:first').val());
+    $('#resStartTime').val($('#resStartTime option:first').val());
+    $('#cateredStartTime').val($('#cateredStartTime option:first').val());
+    $('#hostedType').val($('#hostedType option:first').val());
+    $('#hostedStartTime').val($('#hostedStartTime option:first').val());
+    $('#hostedEndTime').val($('#hostedEndTime option:first').val());
+    $('#careersPosition').val($('#careersPosition option:first').val());
+    if ($("#eventsButtons").hasClass("hidden")) { $("#eventsButtons").removeClass("hidden"); }
+    if ($("#typeOfEvent").hasClass("hidden")) { $("#typeOfEvent").removeClass("hidden"); }
+    if (!$(".catered-event").hasClass("hidden")) { $(".catered-event").addClass("hidden"); }
+    if (!$(".hosted-event").hasClass("hidden")) { $(".hosted-event").addClass("hidden"); }
+  });
+
 });
 
 
@@ -48,5 +90,12 @@ function hostedEvent() {
   $("#eventsButtons").toggleClass("hidden");
   $(".hosted-event").toggleClass("hidden");
   $("#typeOfEvent").toggleClass("hidden");
+}
+
+function clearForms() {
+  $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
+  $(':checkbox, :radio').prop('checked', false);
+  $('select').val($('select option:first').val());
+  $('.total-cost').html('');
 }
 
