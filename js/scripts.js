d87cedf69d1a986e8fe964dc36b20a56bea455b1
midhorner <horner.andrew.e@gmail.com>
2021-03-19 15:32:42 -0400

    gift cards, dropdowns, comments - completed  presentation version

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(function(){
+  // turns burger into X when open
   $(".navbar-toggler").on("click", function(){
     $(".bars").toggleClass("hidden");
     $(".close-icon").toggleClass("hidden");
   });
-
+  // reveals form labels on focus
   $('.form-group :input')
   .on("focus", function() {
       $(this).prev('small').removeClass('hide');
@@ -12,12 +13,13 @@ $(function(){
     $(this).prev('small').addClass('hide');
   });
   
+  // reveals/hides buttons under events menu depending on selection
   $("#cateringButton").on("click", cateredEvent);
   $("#hostingButton").on("click", hostedEvent);
-
   $("#cateredClose").on("click", cateredEvent);
   $("#hostedClose").on("click", hostedEvent);
 
+  // reveals/hides party size warning & checkboxes depending on selection
   $('input[name="rentingRadios"]').on("click", function(){
     let radioValue = $('input[name="rentingRadios"]:checked').val();
     if (radioValue === "yes") {
@@ -26,7 +28,6 @@ $(function(){
       $(".size-warning").addClass("hidden");
     }
   });
-
   $('input[name="cateringRadios"]').on("click", function(){
     let radioValue = $('input[name="cateringRadios"]:checked').val();
     if (radioValue === "on-site") {
@@ -36,6 +37,8 @@ $(function(){
     }
   });
 
+  // logic behind single modal for merch
+  // pulls the necessary info from whichever card is selected
   $('#shopModal').on('show.bs.modal', function (event) {
     const trigger = $(event.relatedTarget);
     let type = trigger.data('type');
@@ -59,6 +62,7 @@ $(function(){
     }
   });
 
+  // clears all form info when the modal is closed
   $('.modal').on('hide.bs.modal', function () {
     $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
     $(':checkbox, :radio').prop('checked', false);
@@ -79,23 +83,24 @@ $(function(){
 
 });
 
-
+// hide/reveal function for catered event
 function cateredEvent() {
   $("#eventsButtons").toggleClass("hidden");
   $(".catered-event").toggleClass("hidden");
   $("#typeOfEvent").toggleClass("hidden");
 }
-
+// hide/reveal button for hosted event
 function hostedEvent() {
   $("#eventsButtons").toggleClass("hidden");
   $(".hosted-event").toggleClass("hidden");
   $("#typeOfEvent").toggleClass("hidden");
 }
 
-function clearForms() {
-  $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
-  $(':checkbox, :radio').prop('checked', false);
-  $('select').val($('select option:first').val());
-  $('.total-cost').html('');
-}
+// first clear form function - doesn't work on modal close -- jquery thing?
+// function clearForms() {
+//   $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
+//   $(':checkbox, :radio').prop('checked', false);
+//   $('select').val($('select option:first').val());
+//   $('.total-cost').html('');
+// }
 
